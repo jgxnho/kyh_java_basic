03b32840cd49b54951c73c23e2e938bf27df8ac8
gxnho <gxnho@naver.com>
2024-01-03 16:32:47 +0900

    1.03 oop

--- src/oop1/MusicPlayerMain2.java
+++ src/oop1/MusicPlayerMain2.java
@@ -7,7 +7,7 @@ public class MusicPlayerMain2 {
         //음악 플레이어 켜기
         data.isOn = true;
         System.out.println("음악 플레이어를 시작합니다");
-        
+
         //볼륨 증가
         data.volume++;
         System.out.println("음악 플레이어 볼륨:"+data.volume);
