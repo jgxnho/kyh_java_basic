54cb4fd75da3e6624d19918c3a97cc6659f7e63d
gxnho <gxnho@naver.com>
2024-01-08 13:46:21 +0900

    1.08 Package

---
+++ src/pack/PackageMain3.java
@@ -0,0 +1,10 @@
+package pack;
+
+import pack.a.User;
+
+public class PackageMain3 {
+    public static void main(String[] args) {
+        User UserA = new User();
+        pack.b.User UserB = new pack.b.User();
+    }
+}
